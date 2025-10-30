30d6b1e58086df1d3f947826cd98b7a987c2d962
socayooot <ngalay2@uccs.edu>
2025-10-30 16:15:41 -0600

    Allowing for Clarice to show up on the Browser using React finishing the tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import "./App.css";
 
-const subject = "React";
-function App() {
+function App(props) {
   return (
     <>
       <header>
-        //Curly braces tell react I want to read the value inside the variable 
+        {/* Curly braces tell react I want to read the value inside the variable  */}
+        {/* This line below allows me to see Clarice from the props section */}
+        <h1>Hello, {props.subject}!</h1>
         {/* Hello, React :)! */}
         {/* $ allows for you to embed things like ) as text */}
-        <h1>Hello, {`${subject} :)`}!</h1>
+        {/* <h1>Hello, {`${subject} :)`}!</h1> */}
         {/* Hello, REACT */}
         {/* Converts the "React" to uppercase */}
-        <h1>Hello, {subject.toUpperCase()}</h1>
+        {/* <h1>Hello, {subject.toUpperCase()}</h1> */}
         {/* Hello, 4! */}
         {/* Adds 2 + 2 and displays the result */}
-        <h1>Hello, {2 + 2}!</h1>
+        {/* <h1>Hello, {2 + 2}!</h1> */}
           <button type="button" className="primary">Click this button</button>
       </header>
     </>
